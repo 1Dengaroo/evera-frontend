adb9d5497bd2480363ca7090f241a5cf75dc5dd7
Andy Deng <andydengaroo@gmail.com>
2024-09-05 20:02:00 -0400

    Setup deployment

--- src/components/Filter/FilterForm.tsx
+++ src/components/Filter/FilterForm.tsx
@@ -1,20 +1,5 @@
 import React from 'react'
-
-interface FilterField {
-  name: string
-  type: 'text' | 'select' | 'date'
-  placeholder?: string
-  options?: { value: string; label: string }[]
-}
-
-interface FilterFormProps {
-  filters: { [key: string]: string }
-  onFilterChange: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => void
-  onFilterSubmit: (e: React.FormEvent) => void
-  fields: FilterField[]
-}
+import { FilterFormProps } from './types'
 
 const FilterForm: React.FC<FilterFormProps> = ({
   filters,
@@ -23,8 +8,8 @@ const FilterForm: React.FC<FilterFormProps> = ({
   fields
 }) => {
   return (
-    <form onSubmit={onFilterSubmit} className="mb-6">
-      <div className="flex space-x-4">
+    <form onSubmit={onFilterSubmit} className="mb-4">
+      <div className="flex space-x-2 items-center">
         {fields.map((field, index) => {
           if (field.type === 'select') {
             return (
@@ -33,7 +18,7 @@ const FilterForm: React.FC<FilterFormProps> = ({
                 name={field.name}
                 value={filters[field.name]}
                 onChange={onFilterChange}
-                className="border px-4 py-2 w-full"
+                className="border px-2 py-1 text-sm w-full"
               >
                 {field.options?.map((option, idx) => (
                   <option key={idx} value={option.value}>
@@ -51,12 +36,15 @@ const FilterForm: React.FC<FilterFormProps> = ({
                 placeholder={field.placeholder}
                 value={filters[field.name]}
                 onChange={onFilterChange}
-                className="border px-4 py-2 w-full placeholder:text-sm"
+                className="border px-2 py-1 text-sm w-full placeholder:text-xs"
               />
             )
           }
         })}
-        <button type="submit" className="bg-black text-white px-4 text-sm">
+        <button
+          type="submit"
+          className="bg-black text-white px-3 py-1 text-xs rounded"
+        >
           Apply
         </button>
       </div>
---
+++ src/components/Filter/types.ts
@@ -0,0 +1,15 @@
+export interface FilterField {
+  name: string
+  type: 'text' | 'select' | 'date'
+  placeholder?: string
+  options?: { value: string; label: string }[]
+}
+
+export interface FilterFormProps {
+  filters: { [key: string]: string }
+  onFilterChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void
+  onFilterSubmit: (e: React.FormEvent) => void
+  fields: FilterField[]
+}
